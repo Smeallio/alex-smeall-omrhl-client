9413358656a65fb1028e25257d4bcc8cb119c555
Smeallio <alexsmeall@gmail.com>
2024-02-01 10:48:41 -0330

    Add homepage styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,34 +8,36 @@ import "./Header.scss";
 const Header = () => {
   return (
     <header className="header">
-      <section className="header__league-logo">
-        <img
-          className="header__league-logo-img"
-          src={omrLogo}
-          alt="Odd Man Rush Hockey League"
-        />
-      </section>
-      <section className="header__team-logos">
-        <img
-          className="header__team-logos-img"
-          src={ducksLogo}
-          alt="Duck Island Saints"
-        />
-        <img
-          className="header__team-logos-img"
-          src={krakenLogo}
-          alt="Kraken Beers"
-        />
-        <img
-          className="header__team-logos-img"
-          src={lepLogo}
-          alt="Fogtown Leprechauns"
-        />
-        <img
-          className="header__team-logos-img"
-          src={mooseLogo}
-          alt="Mighty Moose"
-        />
+      <section className="header__container">
+        <section className="header__league-logo">
+          <img
+            className="header__league-logo-img"
+            src={omrLogo}
+            alt="Odd Man Rush Hockey League"
+          />
+        </section>
+        <section className="header__team-logos">
+          <img
+            className="header__team-logos-img"
+            src={ducksLogo}
+            alt="Duck Island Saints"
+          />
+          <img
+            className="header__team-logos-img"
+            src={krakenLogo}
+            alt="Kraken Beers"
+          />
+          <img
+            className="header__team-logos-img"
+            src={lepLogo}
+            alt="Fogtown Leprechauns"
+          />
+          <img
+            className="header__team-logos-img"
+            src={mooseLogo}
+            alt="Mighty Moose"
+          />
+        </section>
       </section>
     </header>
   );
