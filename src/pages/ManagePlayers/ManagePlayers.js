d0075f4c8dde2c1f967b94cc4e0b2733de6192f0
Smeallio <alexsmeall@gmail.com>
2024-02-03 18:22:17 -0330

    Style the manage players page and install the addPlayer POST functionality with testing to confirm it works

--- src/pages/ManagePlayers/ManagePlayers.js
+++ src/pages/ManagePlayers/ManagePlayers.js
@@ -3,9 +3,11 @@ import Nav from "../../components/Nav/Nav";
 import TeamHeader from "../../components/TeamHeader/TeamHeader";
 import Footer from "../../components/Footer/Footer";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
-import { getPlayersByTeam } from "../../utils/api-utils";
+import { getPlayersByTeam, postPlayer } from "../../utils/api-utils";
 import "./ManagePlayers.scss";
 
 const ManagePlayers = () => {
@@ -39,22 +41,49 @@ const ManagePlayers = () => {
     setTeamId(numTeamId);
   }, [teamName]);
 
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      try {
-        if (teamId !== null) {
-          const response = await axios.get(getPlayersByTeam(teamId));
-          setPlayers(response.data);
-        }
-      } catch (err) {
-        console.log(err.message);
+  const fetchPlayers = async () => {
+    try {
+      if (teamId !== null) {
+        const response = await axios.get(getPlayersByTeam(teamId));
+        setPlayers(response.data);
       }
-    };
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
     fetchPlayers();
   }, [teamId]);
 
   console.log(players);
+  console.log(teamId);
   console.log(getPlayersByTeam(teamId));
+
+  const formRef = useRef();
+
+  const addPlayer = async (event) => {
+    event.preventDefault();
+    const player = {
+      name: formRef.current.name.value,
+      team_id: teamId,
+      position: formRef.current.position.value,
+      number: formRef.current.number.value,
+    };
+    try {
+      await axios.post(postPlayer(teamId), player).then(() => {
+        alert("New Player Added");
+      });
+
+      fetchPlayers();
+
+      formRef.current.name.value = "";
+      formRef.current.position.value = "F";
+      formRef.current.number.value = "";
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   const handleEditClick = (player) => {
     setEditablePlayer({ ...player });
@@ -71,69 +100,94 @@ const ManagePlayers = () => {
       <main className="admin-main">
         <TeamHeader />
         <article className="managePlayers">
-          <table className="managePlayers__table">
-            <caption className="managePlayers__table-title">
-              Roster & Statistics (Skaters)
-            </caption>
-            <thead className="managePlayers__table-headers">
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">No.</th>
-                <th scope="col">Pos.</th>
-                <th scope="col">Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {players.map((player) => (
-                <tr className="managePlayers__table-row" key={player.id}>
-                  <td className="managePlayers__table-box">{player.name}</td>
-                  <td className="managePlayers__table-box">{player.number}</td>
-                  <td className="managePlayers__table-box">
-                    {player.position}
-                  </td>
-                  <td className="managePlayers__table-box">
-                    <button
-                      className="managePlayers__table-box-button"
+          <h3 className="managePlayers__header">Add Players</h3>
+          <form
+            className="managePlayers__add"
+            onSubmit={addPlayer}
+            ref={formRef}
+          >
+            <label className="managePlayers__add-name">
+              <span>Name:</span>
+              <input
+                className="managePlayers__add-name-input"
+                type="text"
+                name="name"
+              ></input>
+            </label>
+            <label className="managePlayers__add-number">
+              <span>Number:</span>
+              <input
+                className="managePlayers__add-number-input"
+                type="number"
+                name="number"
+              ></input>
+            </label>
+            <label className="managePlayers__add-position">
+              <span>Position:</span>
+              <select
+                className="managePlayers__add-position-input"
+                name="position"
+              >
+                <option value="F">F</option>
+                <option value="D">D</option>
+                <option value="G">G</option>
+              </select>
+            </label>
+            <button className="managePlayers__add-button" type="submit">
+              ADD PLAYER
+            </button>
+          </form>
+          <h3 className="managePlayers__header">Edit Players</h3>
+          <section className="managePlayers__edit">
+            <table className="managePlayers__table">
+              <caption className="managePlayers__table-title">
+                Roster & Statistics (Skaters)
+              </caption>
+              <thead className="managePlayers__table-headers">
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">No.</th>
+                  <th scope="col">Pos.</th>
+                  <th scope="col" colSpan="2">
+                    Edit
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {players.map((player) => (
+                  <tr className="managePlayers__table-row" key={player.id}>
+                    <td className="managePlayers__table-box managePlayers__table-box-name">
+                      {player.name}
+                    </td>
+                    <td className="managePlayers__table-box managePlayers__table-box-number">
+                      {player.number}
+                    </td>
+                    <td className="managePlayers__table-box managePlayers__table-position">
+                      {player.position}
+                    </td>
+                    <td className="managePlayers__table-box managePlayers__table-box-edit">
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                      {/* <button
+                      className="managePlayers__table-box-button-edit"
                       onClick={() => handleEditClick(player)}
                     >
-                      Edit Player
-                    </button>
-                  </td>
-                </tr>
-              ))}
-              <tr className="managePlayers__table-row">
-                <td className="managePlayers__table-box">
-                  <input
-                    className="managePlayers__table-box-input"
-                    type="text"
-                    name="name"
-                  ></input>
-                </td>
-                <td className="managePlayers__table-box">
-                  <input
-                    className="managePlayers__table-box-input"
-                    type="text"
-                    name="number"
-                  ></input>
-                </td>
-                <td className="managePlayers__table-box">
-                  <select
-                    className="managePlayers__table-box-input"
-                    name="position"
-                  >
-                    <opton value="F">F</opton>
-                    <opton value="D">D</opton>
-                    <opton value="G">G</opton>
-                  </select>
-                </td>
-                <td className="managePlayers__table-box">
-                  <button className="managePlayers__table-box-button">
-                    Add Player
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                      Edit
+                    </button> */}
+                    </td>
+                    <td className="managePlayers__table-box managePlayers__table-box-delete">
+                      <FontAwesomeIcon icon={faTrashCan} />
+                      {/* <button
+                      className="managePlayers__table-box-button-del"
+                      onClick={() => handleEditClick(player)}
+                    >
+                      Delete
+                    </button> */}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </section>
         </article>
       </main>
       <Footer />
--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -1,3 +1,4 @@
 const API_URL = "http://localhost:5050";
 
 export const getPlayersByTeam = (teamId) => API_URL + "/api/players/" + teamId;
+export const postPlayer = (teamId) => API_URL + "/api/players/" + teamId;
