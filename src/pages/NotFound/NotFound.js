4f200afd395ed6bf9576dfacf966a0392ecb3b5b
Smeallio <alexsmeall@gmail.com>
2024-02-04 23:46:35 -0330

    Add route for page not found

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import Header from "../../components/Globals/Header/Header";
+import Nav from "../../components/Globals/Nav/Nav";
+import Footer from "../../components/Globals/Footer/Footer";
+
+const NotFound = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <p>Page not found</p>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
