43b3cb06a9d6405bd53cda63dd506114ed13bf17
Smeallio <alexsmeall@gmail.com>
2024-03-12 16:58:07 -0230

    Implement lazy loading for hero image

--- src/components/Homepage/Hero/Hero.js
+++ src/components/Homepage/Hero/Hero.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import heroImage from "../../../assets/images/games/Odd-Man-Rush-Hockey-League-Action.webp";
 import "./Hero.scss";
 
 const Hero = () => {
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = heroImage;
+  }, []);
+
+  const backgroundImageStyle = {
+    background: `url(${heroImage})  no-repeat center/cover`
+  }
+
     return (
-          <article className="hero">
+          <article className="hero" style={backgroundImageStyle}>
             <section className="hero__text">
                 <h1 className="hero__h1">Newfoundland & Labrador's First 3 v 3 Ice Hockey League</h1>
             </section>
